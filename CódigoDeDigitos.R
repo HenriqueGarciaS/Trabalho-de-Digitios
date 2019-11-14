77e9f2c7548122689df3d1e3b9f6f65b9783ac5b
HENRIQUE GARCIA SILVA <henrique.gs3@puccampinas.edu.br>
2019-11-14 10:10:54 -0300

    criação do dataframe completa

--- CódigoDeDigitos.R
+++ CódigoDeDigitos.R
@@ -12,22 +12,21 @@ library(e1071)
 library(FNN)
 library(factoextra)
 
-setwd("C:/Users/17079294.LAB-INF.001/Desktop/Trabalho de digitos/digitos")
+#montagem do data frame
+setwd("C:/Users/17079294/Desktop/Trabalho-de-Digitios/digitos")
 data_digitos <- data.frame()
 lista_arquivo <- list.files()
 identificador <- NULL
-i <- 0
 
 for(arquivo in lista_arquivo){
-  print(i)
-  #identificador <- c(identificador,as.numeric(unlist(strsplit(arquivo,'_'))[1]))
+  identificador <- c(identificador,as.numeric(unlist(strsplit(arquivo,'_'))[1]))
   leitura <- read_lines(arquivo)
   leitura <- leitura[-(1:3)]
   leitura <- strsplit(leitura,' ')
   leitura <- unlist(leitura)
   leitura <- as.numeric(leitura)
   data_digitos <- matrix(leitura, nrow = nrow(data_digitos)+1, ncol = length(leitura))
-  i = i + 1
+
 }
 data_digitos <- as.data.frame(data_digitos)
 data_digitos$ident <- identificador
