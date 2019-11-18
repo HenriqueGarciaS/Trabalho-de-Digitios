f4654f4907b0dd7bfbc3f9264c2cc54d56860aa6
HENRIQUE GARCIA SILVA <henrique.gs3@puccampinas.edu.br>
2019-11-18 11:43:46 -0300

    acurácia do svm feita e implementação do kmeans

--- CódigoDeDigitos.R
+++ CódigoDeDigitos.R
@@ -21,20 +21,28 @@ fazKNN <- function(treino,teste,ClasseTreino,classeTeste){
     for(i in  1:length(Resultado))
       if(Resultado[i] == ClasseTeste[i])
         porcentagem <- porcentagem + 1
-    porcentagem <- porcentagem / length(ClasseTeste)*100
+    porcentagem <- (porcentagem / length(ClasseTeste))*100
     print(porcentagem)
   }
 }
 
-fazSVM <- function(treino,teste){
-  classificador <- svm(ident ~ ., treino, 'C-classification', 'linear')
+fazSVM <- function(treino,teste,ClasseTeste){
+  classificador <- svm(ident~ ., treino, 'C-classification', 'linear')
+  porcentagem <- 0
   pred = predict(classificador,teste)
-  pred
-  
+  for(i in  1:length(pred))
+    if(pred[i] == ClasseTeste[i])
+      porcentagem <- porcentagem + 1
+  porcentagem <- (porcentagem / length(ClasseTeste))*100
+  print(porcentagem)
+}
+
+clusteriazacao <- function(data_digitos,k){
+  clusters <- kmeans(data_digitos,k)
 }
 
 #montagem do data frame
-setwd("C:/Users/17079294/Desktop/Trabalho-de-Digitios/digitos")
+setwd("C:/Users/17079294.LAB-INF/Desktop/Trabalho-de-Digitios/digitos")
 data_digitos <- data.frame()
 lista_arquivo <- list.files()
 identificador <- NULL
@@ -57,11 +65,15 @@ smp_size <- floor(0.8*nrow(data_digitos))
 train_ind <- sample(seq_len(nrow(data_digitos)), size = smp_size)
 treino <- data_digitos[train_ind,]
 teste <- data_digitos[-train_ind,]
-ClasseTreino <- treino[,4096]
-ClasseTeste <- teste[,4096]
+ClasseTreino <- treino[,4097]
+ClasseTeste <- teste[,4097]
 
 #KNN
 fazKNN(treino,teste,ClasseTreino,ClasseTeste)
 
 #SVM
-fazSVM(treino,teste)
+fazSVM(treino,teste,ClasseTeste)
+
+#Cluster
+fviz_nbclust(data_digitos,kmeans,method = "wss") +
+  geom_vline(xintercept = 3, linetype = 2)
